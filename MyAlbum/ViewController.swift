030d87b1cf5df2cf507d1b97633778f36d2a5c90
pow2267 <pow2267@gmail.com>
2021-08-16 20:54:58 +0900

    Initial Commit

---
+++ MyAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by kwon on 2021/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

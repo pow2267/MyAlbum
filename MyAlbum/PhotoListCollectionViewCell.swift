7cb818be1531a23eb6b5e720f4ca7b8772bc3c8e
pow2267 <pow2267@gmail.com>
2021-08-18 18:30:31 +0900

    Show albums and make thumbnails

---
+++ MyAlbum/PhotoListCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoListCollectionViewCell.swift
+//  MyAlbum
+//
+//  Created by kwon on 2021/08/17.
+//
+
+import UIKit
+
+class PhotoListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var countLabel: UILabel!
+}
--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -6,14 +6,117 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, PHPhotoLibraryChangeObserver {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var fetchResult: PHFetchResult<PHAssetCollection>!
+    let imageManager: PHCachingImageManager = PHCachingImageManager()
+    
+    func requestCollection() {
+        let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: nil)
+        
+        self.fetchResult = cameraRoll
+    }
+    
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let changes = changeInstance.changeDetails(for: fetchResult) else {
+            return
+        }
+        
+        fetchResult = changes.fetchResultAfterChanges
+        
+        OperationQueue.main.addOperation {
+            //
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell: PhotoListCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? PhotoListCollectionViewCell else {
+            preconditionFailure("오류가 발생했습니다.")
+        }
+        
+        let collections: PHAssetCollection = self.fetchResult[indexPath.row]
+        
+        guard let asset: PHAsset = PHAsset.fetchAssets(in: collections, options: nil).firstObject else {
+            //
+            preconditionFailure()
+        }
+        
+        let imageOption: PHImageRequestOptions = PHImageRequestOptions()
+        
+        imageOption.resizeMode = .exact
+        
+        imageManager.requestImage(for: asset,
+                                  targetSize: CGSize(width: 240, height: 240),
+                                  contentMode: .aspectFill,
+                                  options: imageOption,
+                                  resultHandler: { image, _ in
+                                        cell.imageView.image = image
+        })
+        
+        cell.titleLabel.text = collections.localizedTitle
+        cell.countLabel.text = String(collections.estimatedAssetCount)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.fetchResult.count
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let photoAuthorization = PHPhotoLibrary.authorizationStatus()
+        
+        switch photoAuthorization {
+        case .authorized:
+            print("접근 허가 됨")
+            self.requestCollection()
+            OperationQueue.main.addOperation {
+                self.collectionView.reloadData()
+            }
+        case .limited:
+            print("제한 접근 허가")
+            self.requestCollection()
+            OperationQueue.main.addOperation {
+                self.collectionView.reloadData()
+            }
+        case .denied:
+            print("접근 불허")
+        case .notDetermined:
+            print("아직 응답하지 않음")
+            PHPhotoLibrary.requestAuthorization({ (state) in
+                switch state {
+                case .authorized:
+                    print("사용자가 허가 함")
+                    self.requestCollection()
+                    OperationQueue.main.addOperation {
+                        self.collectionView.reloadData()
+                    }
+                case .limited:
+                    print("제한 접근 허가")
+                    self.requestCollection()
+                    OperationQueue.main.addOperation {
+                        self.collectionView.reloadData()
+                    }
+                case .denied:
+                    print("사용자가 불허 함")
+                default:
+                    break
+                }
+            })
+        case .restricted:
+            print("접근 제한")
+        default:
+            break
+        }
+        
+        // 유저의 모든 Photo Assets을 불러오기 위해
+        PHPhotoLibrary.shared().register(self)
     }
-
-
 }
 

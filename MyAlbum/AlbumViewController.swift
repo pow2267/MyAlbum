0e316d13ae93a94a2b49f539e9d083e1170ec5d1
pow2267 <pow2267@gmail.com>
2021-08-19 22:23:11 +0900

    View photos in specific album when the album is user custom album

--- MyAlbum/AlbumViewController.swift
+++ MyAlbum/AlbumViewController.swift
@@ -6,15 +6,88 @@
 //
 
 import UIKit
+import Photos
 
-class AlbumViewController: UIViewController {
+class AlbumViewController: UIViewController, UICollectionViewDataSource, PHPhotoLibraryChangeObserver {
     
+    @IBOutlet weak var collectionView: UICollectionView!
     var albumTitle: String?
+    var photos: PHFetchResult<PHAssetCollection>?
+    let imageManager: PHCachingImageManager = PHCachingImageManager()
+
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        //
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.photos?.firstObject?.photosCount ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell: ImageCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ImageCollectionViewCell else {
+            preconditionFailure("콜렉션 뷰 셀 생성 오류")
+        }
+        
+        guard let collection = self.photos?.firstObject else {
+            preconditionFailure("앨범 불러오기 오류")
+        }
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        
+        let photo = PHAsset.fetchAssets(in: collection, options: fetchOptions).object(at: indexPath.row)
+        let imageOption: PHImageRequestOptions = PHImageRequestOptions()
+        imageOption.resizeMode = .exact
+        
+        let third: CGFloat = floor((UIScreen.main.bounds.width - 10) / 3.0)
+        
+        imageManager.requestImage(for: photo,
+                                  targetSize: CGSize(width: third, height: third),
+                                  contentMode: .aspectFill,
+                                  options: imageOption,
+                                  resultHandler: { image, _ in
+                                    cell.imageView.image = image
+        })
+        
+        return cell
+    }
+    
+    func requestCollection() {
+        guard let title = self.albumTitle else {
+            preconditionFailure("선택한 앨범을 찾을 수 없음")
+        }
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.predicate = NSPredicate(format: "title = %@", title)
+        
+        let photos: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: fetchOptions)
+        
+        self.photos = photos
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.title = self.albumTitle
+        
+        self.requestCollection()
+        
+        OperationQueue.main.addOperation {
+            self.collectionView.reloadData()
+        }
+        
+        let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumInteritemSpacing = 5
+        flowLayout.minimumLineSpacing = 5
+        
+        let third: CGFloat = floor((UIScreen.main.bounds.width - 10) / 3.0)
+        
+        flowLayout.itemSize = CGSize(width: third, height: third)
+        
+        self.collectionView.collectionViewLayout = flowLayout
+        
+        // 유저의 모든 Photo Assets을 불러오기 위해
+        PHPhotoLibrary.shared().register(self)
     }
     
 
---
+++ MyAlbum/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  MyAlbum
+//
+//  Created by kwon on 2021/08/19.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+}
